f41ea0fc3f459c145386a3e01a6de2eee85cc11c
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2022-10-22 15:27:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Ironfetch
+A work-in-progress program for displaying system information in a fancy way.
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use sysinfo::{System, SystemExt};
+
+const ASCII_ART: &str = include_str!("../ascii_distros/fedora.txt");
+
+fn main() {
+    let sys = System::new_all();
+
+    // Get user info
+    let username = whoami::username();
+    let hostname = whoami::hostname();
+    let user_at_hostname = format!("{}@{}", username, hostname);
+
+    // Generate a separator
+    let mut separator = String::new();
+    for _ in user_at_hostname.chars() {
+        separator.push_str("-")
+    }
+
+    // Get system info
+    let os_name = format!("{} {}", sys.name().unwrap(), sys.os_version().unwrap());
+    let kernel = sys.kernel_version().unwrap();
+    let desktop = whoami::desktop_env().to_string();
+
+    // Collect everything to a vector
+    let printinfo = vec![
+        user_at_hostname,
+        separator,
+        os_name,
+        kernel,
+        desktop,
+    ];
+
+    // Print everything
+    let ascii_arr: Vec<&str> = ASCII_ART.split("\n").collect();
+    for (i, value) in (&printinfo).into_iter().enumerate() {
+        println!("{} {}", ascii_arr[i], value);
+    }
+
+    // Print the rest of the ASCII
+    for line in (&printinfo).len()..ascii_arr.len() {
+        println!("{}", ascii_arr[line]);
+    }
+}
