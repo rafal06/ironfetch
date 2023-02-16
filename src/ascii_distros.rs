1c808bb0c1e52463a2a1824d2bf9487e800e9b50
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2023-02-16 10:43:32 +0100

    Add openSUSE Tumbleweed logo

--- src/ascii_distros.rs
+++ src/ascii_distros.rs
@@ -7,6 +7,7 @@ pub static ASCII_DISTROS: Map<&'static str, &'static str> = phf_map! {
     "debian"      => include_str!("../ascii_distros/debian.txt"      ),
     "endeavouros" => include_str!("../ascii_distros/endeavouros.txt" ),
     "fedora"      => include_str!("../ascii_distros/fedora.txt"      ),
+    "opensuse-tumbleweed"      => include_str!("../ascii_distros/opensuse-tumbleweed.txt"      ),
     "raspbian"    => include_str!("../ascii_distros/raspbian.txt"    ),
     "linux"       => include_str!("../ascii_distros/linux.txt"       ),
 };
